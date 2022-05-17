2a526b167c92b7f5cf8983d4b54c9a98a1d4cbca
akshat-vyas <105382249+akshat-vyas@users.noreply.github.com>
2022-05-17 23:54:11 +0530

    Create factRecurrsion.py

---
+++ factRecurrsion.py
@@ -0,0 +1,11 @@
+#factorial of number using recursion
+def fact(x):
+ y=1
+ if x==1 or x==0:
+ return y
+ else:
+ y=x*fact(x-1)
+ return y
+val=int(input("enter a value: "))
+funct_val=fact(val)
+print("factorial of {} is {}".format(val,funct_val))
