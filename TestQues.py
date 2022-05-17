bcac8d30310ae9089e12ce09f09f6feaf3680728
akshat-vyas <105382249+akshat-vyas@users.noreply.github.com>
2022-05-17 21:35:30 +0530

    Create TestQues.py

---
+++ TestQues.py
@@ -0,0 +1,34 @@
+# TEST QUESTION
+"""word="I am fine"
+n=len(word)
+word1=word.upper()
+word2=word.lower()
+converted_word=""
+print(len(word))
+for i in range(n):
+ if i%2==0:
+ converted_word+=word2[i]
+ else:
+ converted_word+=word1[i]
+print(converted_word)"""
+"""x="kacha badam"
+for i in range(len(x)-1,-1,-1):
+ print(i)
+ print(x[i])"""
+"""name=input("what is your name: ")
+age=input("how old are you: ")
+yera=str((2022-age)+100)
+print(name+"will be 100 year old in the year "+year)"""
+"""n=5
+k=5
+for i in range(0,n+1):
+ for j in range(k-i,0,-1):
+ print(j,end='')
+ print()"""
+"""n=int(input("enter number"))
+sum=0
+for num in range(1,n+1,1):
+ sum=sum+num
+print("sum of first",n,"number is:",sum)
+average=sum/n
+print("average of",n,"numbers is:",average)"""
