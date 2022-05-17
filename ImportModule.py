571a3a9c1bab20376eb4f997b8da98282b7ca953
akshat-vyas <105382249+akshat-vyas@users.noreply.github.com>
2022-05-17 23:08:09 +0530

    Create ImportModule.py

---
+++ ImportModule.py
@@ -0,0 +1,19 @@
+# IMPORTING MODULE
+#USING import
+"""import math
+x=4
+y=math.sqrt(x)
+print(x)
+print(y)"""
+#USING import as
+"""import math as m
+x=4
+y=m.sqrt(x)
+print(x)
+print(y)"""
+#USING from __input
+"""from math import sqrt # if we want all function then use * in place
+of sqrt
+x=4
+y=sqrt(x)
+print(x,y)"""
