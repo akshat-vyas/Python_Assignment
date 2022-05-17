8a4c9bb3759bd1885195789cc0dc4b8c2e9c8413
akshat-vyas <105382249+akshat-vyas@users.noreply.github.com>
2022-05-17 20:03:42 +0530

    Create Class.py

---
+++ Class.py
@@ -0,0 +1,11 @@
+#class in python
+class circle:
+ def __init__(self,radius):
+ self.radius=radius
+ def area(self):
+ return 3.14*self.radius*self.radius
+ def circumference(self):
+ return 2*3.14*self.radius
+c1=circle(2)
+print(c1.area())
+print(c1.circumference())
