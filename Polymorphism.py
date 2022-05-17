4cc48913a02958c399d240f9dfae007aca7db8cb
akshat-vyas <105382249+akshat-vyas@users.noreply.github.com>
2022-05-17 23:51:37 +0530

    Create Polymorphism.py

---
+++ Polymorphism.py
@@ -0,0 +1,25 @@
+#polymorphism in class methods
+class Cat:
+ def _init_(self, name, age):
+ self.name = name
+ self.age = age
+ def info(self):
+ print(f"I am a cat. My name is {self.name}. I am {self.age} years
+old.")
+ def make_sound(self):
+ print("Meow")
+class Dog:
+ def _init_(self, name, age):
+ self.name = name
+ self.age = age
+ def info(self):
+ print(f"I am a dog. My name is {self.name}. I am {self.age} years
+old.")
+ def make_sound(self):
+ print("Bark")
+cat1 = Cat("Kitty", 2.5)
+dog1 = Dog("Fluffy", 4)
+for animal in (cat1, dog1):
+ animal.make_sound()
+ animal.info()
+ animal.make_sound()
