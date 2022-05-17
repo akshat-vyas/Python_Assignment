c5ced6fc9f3321dff7a1d1eb2981085a2038feb1
akshat-vyas <105382249+akshat-vyas@users.noreply.github.com>
2022-05-17 23:34:08 +0530

    Create List.py

---
+++ List.py
@@ -0,0 +1,30 @@
+#LIST
+"""a=[1,2,3,4,5,6,7,8,9]
+print(a)
+b=['Akshat',"Poorv",56,2.3]
+print(b)
+print(type(a))
+print(type(b))
+c=list("Akshat")
+print(c)"""
+# APPEND
+"""a=[1,2,3]
+print(a)
+a.append(23)
+print(a)"""
+#INSERT
+"""b=[1,2,3]
+print(b)
+b.insert(2,26)
+print(b)"""
+"""a=["riya","Jawahar Nagar",6260345739,"akshat2@gmail.com"]
+print(a)
+print("NAME: ",a[0])
+print("ADDRESS: ",a[1])
+print("CONTACT: ",a[2])
+print("EMAIL: ",a[3])"""
+a=[1,2,3,4,5,6,7,8,9,10]
+for i in range(len(a)):
+ print(a[i])
+x=a.index(5)
+print(x)
