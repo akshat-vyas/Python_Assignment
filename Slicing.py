a359605f2e10155636d3aaf2e91e7c8291ef0a56
akshat-vyas <105382249+akshat-vyas@users.noreply.github.com>
2022-05-19 16:04:30 +0530

    Create Slicing.py

---
+++ Slicing.py
@@ -0,0 +1,12 @@
+# slicing
+txt="Hello, World!"
+print(txt[2:5])
+# slice from the start
+txt="Hello, World!"
+print(txt[:5])
+# slice to the end
+txt="Hello, World!"
+print(txt[2:])
+#negative indexing
+txt="Hello, World!"
+print(txt[-5:-2])
